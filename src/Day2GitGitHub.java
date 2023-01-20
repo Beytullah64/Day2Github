0f2604ca930239c3d2adbcb4a44363a7a76ab829
Beytullah Tuncer <tuncerbeytullah6464@gmail.com>
2023-01-20 15:59:56 +0300

    v3

---
+++ src/Day2GitGitHub.java
@@ -0,0 +1,9 @@
+public class Day2GitGitHub {
+    /*
+    git push --> uzak repoya (yani remote - Github)
+    göndermek için kullanddigimiz kod yanlız git push komutunu direk ullanmak istersek 1 kez
+                git remote add orgin adres
+                git push -uorgin master
+    Not: Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci komitler için sadece git push kullaniriz
+     */
+}
