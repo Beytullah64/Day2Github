56515290c1177d6556b7188d636e29a514beb74c
Beytullah Tuncer <tuncerbeytullah6464@gmail.com>
2023-01-20 16:07:58 +0300

    v4

--- src/Day2GitGitHub.java
+++ src/Day2GitGitHub.java
@@ -3,7 +3,10 @@ public class Day2GitGitHub {
     git push --> uzak repoya (yani remote - Github)
     göndermek için kullanddigimiz kod yanlız git push komutunu direk ullanmak istersek 1 kez
                 git remote add orgin adres
-                git push -uorgin master
+                git push -u orgin master
     Not: Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci komitler için sadece git push kullaniriz
      */
+    public static void main(String[] args) {
+        System.out.println("Github ı da öğrendim");
+    }
 }
