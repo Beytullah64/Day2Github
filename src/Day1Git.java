b740d26219fb91c24a710f6d2a242764e82365da
Beytullah Tuncer <tuncerbeytullah6464@gmail.com>
2023-01-21 16:23:33 +0300

    C.1

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,6 +1,9 @@
+package Day2Github.src;
+
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1. Deneme");
         System.out.println("Hello Git 2. Deneme");
+        System.out.println("Hello Git 3. Deneme");
     }
 }
