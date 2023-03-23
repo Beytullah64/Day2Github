8a5e230638b887e0240cc2065e1dfe7e9dba609b
Beytullah64 <122700605+Beytullah64@users.noreply.github.com>
2023-03-24 00:17:25 +0300

    Update Day1Git2.java

--- src/Day1Git2.java
+++ src/Day1Git2.java
@@ -4,5 +4,6 @@ public class Day1Git2 {
     public static void main(String[] args) {
         System.out.println("Git 2. class");
         System.out.println("Git 2. class");
+        System.out.println("Git 2. class");
     }
 }
