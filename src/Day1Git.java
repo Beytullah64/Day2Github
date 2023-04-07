24b3e5d3266fb1a38a58f681d8181464c988176f
Beytullah64 <122700605+Beytullah64@users.noreply.github.com>
2023-04-08 02:56:02 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -7,6 +7,12 @@ public class Day1Git {
         System.out.println("Hello Git 3. Deneme");
         System.out.println("Hello Git 4. Deneme");
         System.out.println("Hello Git 5. Deneme");
+        
+         System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 2. Deneme");
+        System.out.println("Hello Git 3. Deneme");
+        System.out.println("Hello Git 4. Deneme");
+        System.out.println("Hello Git 5. Deneme");
     
         
     }
